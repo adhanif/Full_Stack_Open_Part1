d39286ff2e67cf303348d5ca5706368b97539127
ad.hanif <ad.hanif@outlook.com>
2023-08-23 23:10:21 +0200

    Exercice 1.11 unicafe step6  completed

--- unicafe/src/components/Statistics.jsx
+++ unicafe/src/components/Statistics.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import StatisticLine from "./StatisticLine";
 
 export default function Statistics({ good, neutral, bad }) {
   let total = good + neutral + bad;
+  let average = (good - bad) / total;
+  let positive = (good / total) * 100;
   return (
     <div>
       {total ? (
         <>
           <h1>Statistics</h1>
-          <p>good {good}</p>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={`${positive} %`} />
+          {/* <p>good {good}</p>
           <p>neutral {neutral}</p>
           <p>bad {bad}</p>
           <p>all {total}</p>
-          <p>average {(good - bad) / total}</p>
-          <p>positive {(good / total) * 100} %</p>
+          <p>average {}</p>
+          <p>positive {} %</p> */}
         </>
       ) : (
         <>
